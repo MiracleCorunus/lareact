c333ec5b11c9171cb911a9a28b188aa7530e8838
Alex Turysta <italexx.ua@gmail.com>
2021-05-24 16:09:42 +0900

    Handling click events

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-
+import Loading from './Loading'
 class App extends Component {
   constructor(props) {
     super(props);
@@ -8,6 +8,7 @@ class App extends Component {
       users: [],
       loading: false,
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   getUsers() {
@@ -17,12 +18,18 @@ class App extends Component {
 
     axios('https://api.randomuser.me/?nat=US&results=5')
       .then(response => this.setState({
-        users: response.data.results,
+        users: [...this.state.users, ...response.data.results],
         loading: false
       }));
   }
   componentWillMount() {
     this.getUsers();
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.getUsers();
+    console.log('more users loaded');
   }
 
   render() {
@@ -34,9 +41,12 @@ class App extends Component {
               <h3>{user.name.first}</h3>
               <p>{user.email}</p>
               <hr />
+              <form onSubmit={this.handleSubmit}>
+                <input type="submit" value="load users" />
+              </form>
             </div>
           )) :
-          "Loading..."
+          <Loading />
         }
       </div>
     )
